863bda576548e15fd8bbcc8cd40b577e6b583cb2
Nate Hawthorne <natejhawthorne@gmail.com>
2016-03-07 13:09:23 -0600

    Made a little madlib. Hope you enjoy.

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var noun = prompt("Please enter a noun:");
+var verb = prompt("Please enter a verb:");
+var adjective = prompt("Please enter an adjective:");
+var adverb = prompt("Please enter an adverb:");
+var noun2 = prompt("Please enter another noun:");
+var noun3 = prompt("Please enter one more noun:");
+var adjective2 = prompt("Please enter another adjective:");
+var verb2 = prompt("Please enter another verb:");
+
+console.log("Do you "+verb+" your "+adjective+" "+noun+" "+adverb+"? That's hilarious! I have a "+adjective+" "+noun2+" that I like to "+verb+" with, and a "+adjective2+" "+noun3+" that I like to "+verb2+" with. Small world..");
